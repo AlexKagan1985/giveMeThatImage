1264c5af4496274924594ea90e61689af7406b5c
Jagholin <pavel.belskiy2@gmail.com>
2023-02-16 11:05:23 +0100

    some basic backend stuff

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+
+const { router: indexRouter } = require('./routes/index');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/', indexRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
