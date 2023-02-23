f86f68d8af91f97787d5f4a8e192392530364bf7
Alex Kagan <alexkagan985@gmail.com>
2023-02-23 10:14:27 +0100

    added basic profile component lay out

--- frontend/src/components/Navigationbar.jsx
+++ frontend/src/components/Navigationbar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import Profile from "./Profile";
+// import Profile from "./Profile";
+import Avatar from "react-avatar";
 
 function Navigationbar() {
   return (
@@ -17,7 +18,11 @@ function Navigationbar() {
             <Nav.Link href="#/action-3">Log out</Nav.Link>
           </Navbar.Offcanvas>
         </Navbar.Collapse>
-        <Profile />
+        <Avatar
+          size="50"
+          facebook-id="invalidfacebookusername"
+          src="http://www.gravatar.com/avatar/a16a38cdfe8b2cbd38e8a56ab93238d3"
+        />
       </Navbar>
     </>
   );
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,14 +1,65 @@
 import React from "react";
-import Avatar from "react-avatar";
+
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function Profile() {
   return (
     <div>
-      <Avatar
-        size="50"
-        facebook-id="invalidfacebookusername"
-        src="http://www.gravatar.com/avatar/a16a38cdfe8b2cbd38e8a56ab93238d3"
-      />
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">text</Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="confirm-password"
+            placeholder="Confirm Password"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Login/User Name</Form.Label>
+          <Form.Control type="confirm-password" placeholder="Login/User Name" />
+        </Form.Group>
+
+        <Form.Group className="position-relative mb-3">
+          <Form.Label>File</Form.Label>
+          <Form.Control
+            type="file"
+            required
+            name="file"
+            // onChange={handleChange}
+            // isInvalid={!!errors.file}
+          />
+          <Form.Control.Feedback type="invalid" tooltip>
+            {/* {errors.file} */}
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Label htmlFor="text">Short description</Form.Label>
+        <Form.Control type="text" id="text" placeholder="Please Enter text" />
+        <Form.Text id="passwordHelpBlock" muted>
+          please provide a short description about yourself
+        </Form.Text>
+
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
