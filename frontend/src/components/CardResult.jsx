2fd0480eff4436b3e42ae796f39e82cb9fbc728b
Alex Kagan <alexkagan985@gmail.com>
2023-02-24 10:32:29 +0100

    small styling changes

--- frontend/src/components/CardResult.jsx
+++ frontend/src/components/CardResult.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import testData from "../mock_data/out.json";
+import classes from "./CardResult.module.css";
 
 function CardResult() {
   return testData.map((data) => (
     <Card style={{ width: "18rem" }} key={data.id}>
-      <Card.Img variant="top" src={data.preview_url} />
-      <Card.Body>
+      <Card.Img variant="top" src={data.preview_url} className={classes.img} />
+      <Card.Body className={classes.body}>
         <Card.Title>{data.title}</Card.Title>
       </Card.Body>
       <ListGroup className="list-group-flush">
@@ -15,7 +16,7 @@ function CardResult() {
           Object.keys(data.api_data)[0]
         }`}</ListGroup.Item>
       </ListGroup>
-      <Card.Body>
+      <Card.Body className={classes.body}>
         <Card.Link href={data.img_url}>Card Link</Card.Link>
       </Card.Body>
     </Card>
