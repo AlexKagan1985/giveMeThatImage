617c41a9ba9fb2cca19353c5b435068263aeea5f
Alex Kagan <Alexkagan1985@gmail.com>
2023-02-27 17:40:00 +0100

    changed the pagination

--- frontend/src/components/CardResult.jsx
+++ frontend/src/components/CardResult.jsx
@@ -6,15 +6,52 @@ import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 // eslint-disable-next-line no-unused-vars
 import { PaginatedSearchResult } from "../atoms/search";
-import testData from "../mock_data/out.json";
 import classes from "./CardResult.module.css";
 
+const Paginated = ({ totalPages, setCurrentPage, currentPage }) => {
+  const results = [];
+
+  if (totalPages < 7) {
+    for (let i = 1; i < totalPages; i++) {
+      results.push(
+        <Pagination.Item onClick={() => setCurrentPage(i)}> i </Pagination.Item>
+      );
+    }
+  } else {
+    results.push(
+      <>
+        <Pagination.First />
+        <Pagination.Prev />
+        <Pagination.Item onClick={() => setCurrentPage(1)}>{1}</Pagination.Item>
+        <Pagination.Ellipsis />
+
+        {Array.from({ length: 5 }).map((_, idx) => (
+          <Pagination.Item
+            key={idx}
+            onClick={() => setCurrentPage(currentPage - idx + 3)}
+          >
+            {currentPage - idx + 3}
+          </Pagination.Item>
+        ))}
+
+        <Pagination.Ellipsis />
+        <Pagination.Item onClick={() => setCurrentPage(10)}>
+          {10}
+        </Pagination.Item>
+        <Pagination.Next />
+        <Pagination.Last />
+      </>
+    );
+  }
+  return results;
+};
+
 /**
-  * @typedef {object} CardResultProps
-  * @property {PaginatedSearchResult} currentData
-  *
-  * @param {CardResultProps} param
-  */
+ * @typedef {object} CardResultProps
+ * @property {PaginatedSearchResult} currentData
+ *
+ * @param {CardResultProps} param
+ */
 function CardResult({ currentData }) {
   const [currentPage, setCurrentPage] = useState(1);
   const pageAtom = useMemo(() => {
@@ -28,24 +65,32 @@ function CardResult({ currentData }) {
 
   return (
     <>
-      {pageData.state === "hasData" ? pageData.data.map((data) => (
-        <Card style={{ width: "18rem" }} key={data.id}>
-          <Card.Img variant="top" src={data.preview_url} className={classes.img} />
-          <Card.Body className={classes.body}>
-            <Card.Title>{data.title}</Card.Title>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item>{`providor: ${currentData.provider}`}</ListGroup.Item>
-          </ListGroup>
-          <Card.Body className={classes.body}>
-            <Card.Link href={data.img_url}>Card Link</Card.Link>
-          </Card.Body>
-        </Card>
-      )) : "Loading..."}
+      {pageData.state === "hasData"
+        ? pageData.data.map((data) => (
+            <Card style={{ width: "18rem" }} key={data.id}>
+              <Card.Img
+                variant="top"
+                src={data.preview_url}
+                className={classes.img}
+              />
+              <Card.Body className={classes.body}>
+                <Card.Title>{data.title}</Card.Title>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroup.Item>{`providor: ${currentData.provider}`}</ListGroup.Item>
+              </ListGroup>
+              <Card.Body className={classes.body}>
+                <Card.Link href={data.img_url}>Card Link</Card.Link>
+              </Card.Body>
+            </Card>
+          ))
+        : "Loading..."}
       <Pagination>
-        <Pagination.Item onClick={() => setCurrentPage(1)}> 1 </Pagination.Item>
-        <Pagination.Item onClick={() => setCurrentPage(2)}> 2 </Pagination.Item>
-        <Pagination.Item onClick={() => setCurrentPage(3)}> 3 </Pagination.Item>
+        <Paginated
+          totalPages={currentData.totalPages}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        />
       </Pagination>
     </>
   );
