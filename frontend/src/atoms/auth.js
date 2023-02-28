485b89e9b2772b3ff8ff756dee318e9ced9f47f2
Jagholin <pavel.belskiy2@gmail.com>
2023-02-28 15:05:37 +0100

    fix dont have user -> no token bug

--- frontend/src/atoms/auth.js
+++ frontend/src/atoms/auth.js
@@ -2,5 +2,5 @@ import { atom } from "jotai";
 
 export const loggedInUser = atom(null);
 export const loggedInUserToken = atom((get) => {
-    return get(loggedInUser).token;
+  return get(loggedInUser)?.token;
 });
