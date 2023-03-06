931533ab5f5cca233c55275e0fa7fda73318e5d3
Alex Kagan <Alexkagan1985@gmail.com>
2023-03-06 12:31:12 +0100

    changed the nav and serch bars stylings

--- frontend/src/components/Navigationbar.jsx
+++ frontend/src/components/Navigationbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Avatar from "react-avatar";
 import classes from "./Navigationbar.module.scss";
 import { NavLink, useLocation } from "react-router-dom";
 import { useAtomValue } from "jotai";
 import { loggedInUser, logOut } from "../atoms/auth";
+import { Button } from "react-bootstrap";
 
 function Navigationbar() {
   const theUser = useAtomValue(loggedInUser);
@@ -14,49 +14,69 @@ function Navigationbar() {
   const handleLogout = (e) => {
     e.preventDefault();
     logOut();
-  }
+  };
 
   return (
     <>
       <Navbar className="fixed-top" bg="light">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <NavLink className="nav-link" to="/" >
-            <Nav.Link className={classes.link} as="span">
-              Home
-            </Nav.Link>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? classes.activeNavlink : classes.nonActiveNavlink
+            }
+            to="/"
+          >
+            Home
           </NavLink>
-          <NavLink className="nav-link" to="/user-history" >
-            <Nav.Link className={classes.link} as="span">
-              History
-            </Nav.Link>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? classes.activeNavlink : classes.nonActiveNavlink
+            }
+            to="/user-history"
+          >
+            History
           </NavLink>
-          <Nav.Link className={classes.link} as="span">
-            Favorites
-          </Nav.Link>
-          {theUser && <NavLink className="nav-link" to="/profile" >
-            <Nav.Link className={classes.link} as="span">
+
+          {theUser && (
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? classes.activeNavlink : classes.nonActiveNavlink
+              }
+              to="/profile"
+            >
               Profile
-            </Nav.Link>
-          </NavLink>}
-          {!theUser && <NavLink className="nav-link" to={`/login/${encodeURIComponent(currentLocation.pathname)}`} >
-            <Nav.Link className={classes.link} as="span">
+            </NavLink>
+          )}
+          {!theUser && (
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? classes.activeNavlink : classes.nonActiveNavlink
+              }
+              to={`/login/${encodeURIComponent(currentLocation.pathname)}`}
+            >
               Log in
-            </Nav.Link>
-          </NavLink>}
-          {theUser && <Nav.Link className={classes.link} onClick={handleLogout}>
-            Log out
-          </Nav.Link>}
+            </NavLink>
+          )}
+          {theUser && (
+            <Button variant="light" onClick={handleLogout}>
+              Log out
+            </Button>
+          )}
         </Navbar.Collapse>
         <div className={classes.user_info}>
           <div className={classes.user_name}>{theUser && theUser.login}</div>
           <div>{theUser && theUser.description}</div>
         </div>
-        {theUser && <Avatar
-          size="50"
-          round={true}
-          name={theUser.login}
-        />}
+        {theUser && (
+          <Avatar
+            size="50"
+            round={true}
+            name={theUser.login}
+            padding="2rem"
+            className={classes.avatar}
+          />
+        )}
       </Navbar>
     </>
   );
